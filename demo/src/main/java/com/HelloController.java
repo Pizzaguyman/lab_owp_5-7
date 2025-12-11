956aff73ad8c0b735b7cf5d46a1f79e47e86a4ea
pizzaguyguy <mihan02032006@mail.ru>
2025-12-11 21:00:50 +0300

    init

---
+++ demo/src/main/java/com/HelloController.java
@@ -0,0 +1,17 @@
+package com;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HelloController {
+
+	@GetMapping
+	public String index(Model model) {
+		model.addAttribute("msg","Hello!");
+		return "index";
+	}
+}
+
