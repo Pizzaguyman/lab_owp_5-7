547428fb168a119b6daf0c576d4661012b62bd66
pizzaguyguy <mihan02032006@mail.ru>
2025-12-12 18:30:39 +0300

    Switched from a test project

--- src/test/java/com/not_a_team/university/UniversityApplicationTests.java
+++ src/test/java/com/not_a_team/university/UniversityApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.not_a_team.university;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class UniversityApplicationTests {
 
 	@Test
 	void contextLoads() {
